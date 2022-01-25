13bb04781404898446e1e09d3dd2d708c19dffef
GIT-KINDMAN <mankind1990s@gmail.com>
2022-01-25 17:19:59 +0900

    commit

--- 1_Java/workspace/day02_basic/day02_basic/src/App.java
+++ 1_Java/workspace/day02_basic/day02_basic/src/App.java
@@ -1,7 +1,7 @@
-package ex01.basic;
+/*package ex01.basic;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
-}
+}*/
