886bf6a30f8582c186b2b00e6509504c28d73310
GIT-KINDMAN <mankind1990s@gmail.com>
2022-01-25 11:48:02 +0900

    commit

--- 1_Java/workspace/Hello.java
+++ 1_Java/workspace/Hello.java
@@ -1,5 +1,9 @@
-class Hello {
+class Hello extends Object {
     public static void main(String[] args) {
-        
+        System.out.println("Hello");
+        System.out.println("\n한\t\t\t글");
+        System.out.print("A\t");
+        System.out.print(100);
+        System.out.println("\t kosa");
     }
 }
---
+++ 1_Java/workspace/info.java
@@ -0,0 +1,7 @@
+public class info {
+    public static void main(String[] args) {
+        System.out.println("이름: 이동훈");
+        System.out.println("연락처: 010-8371-3282");
+        System.out.println("주소: 서울시 강동구");
+    }
+}
