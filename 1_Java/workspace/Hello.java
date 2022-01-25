745a6d582b7ae4d23decc7cf40a1f95517c70bce
GIT-KINDMAN <mankind1990s@gmail.com>
2022-01-25 12:07:36 +0900

    commit

--- 1_Java/workspace/Hello.java
+++ 1_Java/workspace/Hello.java
@@ -2,8 +2,8 @@ class Hello extends Object {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("\n한\t\t\t글");
-        System.out.print("A\t");
+        System.out.print("A ");
         System.out.print(100);
-        System.out.println("\t kosa");
+        System.out.println(" kosa");
     }
 }
